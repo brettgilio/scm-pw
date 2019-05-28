f7592a9e34a7b62f23d77010a8a655549892dbcb
Brett Gilio <brettg@posteo.net>
2019-05-28 16:34:50 -0500

    Update haunt.scm: Add definitions for blog and stylesheets

--- haunt.scm
+++ haunt.scm
@@ -24,3 +24,25 @@
              (ice-9 regex)
              (ice-9 match)
              (web uri))
+
+(define (date year month day)
+  "Create a SRFI-19 date for the given YEAR, MONTH, DAY"
+  (let ((tzoffset (tm:gmtoff (localtime (time-second (current-time))))))
+    (make-date 0 0 0 0 day month year tzoffset)))
+
+(define (stylesheet name)
+  `(link (@ (rel "stylesheet")
+            (href ,(string-append "/css/" name ".css")))))
+
+(define* (anchor content #:optional (uri content))
+  `(a (@ (href ,uri)) ,content))
+
+(define %cc-by-sa-link
+  '(a (@ (href "https://creativecommons.org/licenses/by-sa/4.0/"))
+      "Creative Commons Attribution Share-Alike 4.0 International"))
+
+(define %cc-by-sa-button
+  '(a (@ (class "cc-button")
+         (href "https://creativecommons.org/licenses/by-sa/4.0/"))
+      (img (@ (src "https://licensebuttons.net/l/by-sa/4.0/80x15.png")))))
+
